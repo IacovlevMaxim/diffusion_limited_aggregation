be5a87b40534b39fe47d1b63db110b5200748eb3
IacovlevMaxim <iacovlev.maxim@gmail.com>
2024-06-12 22:18:31 +0300

    Added parameters and GIF download to Coral Growth

--- presets/coral_growth/sketch.js
+++ presets/coral_growth/sketch.js
@@ -1,21 +1,72 @@
 let coordinates = [];
-let space = 5;
+const space = 5;
 let numx;
 let numy;
 let Ccolor = [];
 let isRecording = false;
+let frameSlider;
+let meanSlider;
+let biasSlider;
+let frames;
+let mean;
+let bias;
+let recFrames = 1000;
+let recSlider;
+const diffusion = 20;
 
 function setup() {
-  createCanvas(640, 640);
+  const cnv = createCanvas(640, 640);
   const button = document.getElementById("download");
-  button.addEventListener("click", () => {
-      button.textContent = "Download GIF";
-      downloadGif();
-  });
+  button.addEventListener("click", () => downloadGif(recFrames));
+
+  cnv.parent("dropdown");
+
+  recFrames = 1000;
+  recSlider = new CustomSlider("Record Frames", 0, 0, 100, 1800, recFrames);
+  recSlider.init();
+  recSlider.onUpdate = () => {
+    if (recSlider.getValue() != recFrames) {
+      console.log(recSlider.getValue());
+      recFrames = recSlider.getValue();
+    }
+  };
+
+  frames = 30;
+  frameRate(frames);
+  frameSlider = new CustomSlider("Frame Rate", 0, 0, 1, 60, frames);
+  frameSlider.init();
+  frameSlider.onUpdate = () => {
+    if (frameSlider.getValue() != frames) {
+      console.log(frameSlider.getValue());
+      frameRate(frameSlider.getValue());
+      frames = frameSlider.getValue();
+    }
+  };
+
+  mean = 0.5
+  meanSlider = new CustomSlider("Mean Slider", 0, 0, 0, 1, mean, 0.01);
+  meanSlider.init();
+  meanSlider.onUpdate = () => {
+    if (meanSlider.getValue() != mean) {
+      mean = meanSlider.getValue();
+    }
+  };
+
+  bias = 0.1
+  biasSlider = new CustomSlider("Deviation Slider", 0, 0, 0, 0.5, bias, 0.01);
+  biasSlider.init();
+  biasSlider.onUpdate = () => {
+    if (biasSlider.getValue() != bias) {
+      bias = biasSlider.getValue();
+    }
+  };
+
   numx = int(width / space) + space;
   console.log("numx ", numx);
   numy = int(height / space);
   console.log("numy ", numy);
+
+  attractor = width / 2;
 
   for (var x = 0; x < numx; x++) {
     coordinates[x] = []; // create nested array
@@ -29,11 +80,29 @@ function setup() {
   }
 }
 
+function pseudorandom(mean, bias) {
+  const r = random();
+  const b = mean + bias / 1.5;
+  const a = mean - bias / 1.5;
+  if(r < mean + bias && r > mean - bias) return random() * (b-a) + a
+
+  return r;
+  
+}
+
 function draw() {
-  if (isRecording) {
   background(0);
+  frameSlider.update();
+  meanSlider.update();
+  biasSlider.update();
+  recSlider.update();
+
   for (let cycles = 0; cycles < 4; cycles++) {
-    let xcor = int(random(1, numx - 3));
+    let xcor = int(map(pseudorandom(mean, bias), 0, 1, 1, numx - 4));
+    if(xcor > 131) xcor = 131;
+    if(xcor < 2) xcor = 2
+
+    console.log(xcor);
     for (let i = 0; i < numy; i++) {
       const inheritedColorIndices = [
         [xcor - 1, i],
@@ -49,9 +118,9 @@ function draw() {
         const inheritedColor = Ccolor[x_ind][y_ind];
 
         Ccolor[xcor][i - 1] = color(
-          red(inheritedColor) + random(-20, 20),
-          green(inheritedColor) + random(-20, 20),
-          blue(inheritedColor) + random(-20, 20)
+          red(inheritedColor) + random(-diffusion, diffusion),
+          green(inheritedColor) + random(-diffusion, diffusion),
+          blue(inheritedColor) + random(-diffusion, diffusion)
         );
 
         i = numy + 1;
@@ -72,16 +141,4 @@ function draw() {
         }
       }
     }
-  }
-}
-
-function downloadGif() {
-  if (isRecording) {
-    // Stop recording if already recording    
-    saveGif("myAnimation.gif", frameCount / 60); // Download GIF with duration based on frame count
-    isRecording = false;
-  } else {
-    // Start recording if not already recording
-    isRecording = true;
-  }
 }
